5515494d5d5838b41cfa8c8ed1148abe626e1cc5
EsadSancaktutan <60690338+EsadSancaktutan@users.noreply.github.com>
2020-04-20 02:10:31 +0300

    JAVA PROJESI YÜKLENDI

---
+++ README.md
@@ -0,0 +1,9 @@
+# JAVA PROJESÝ | CarProject
+
+## Projenin çalýþabilmesi için
+* admin.sql dosyasý import edilmesi gerekmektedir.
+* "mysql-connector-java-5.1.48.jar" dosyasý projeye dahil edilmelidir.
+
+
+### Muhammed Esad Sancaktutan
+### 20.04.2020
---
+++ admin.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 20 Nis 2020, 01:04:37
+-- Sunucu sürümü: 10.4.11-MariaDB
+-- PHP Sürümü: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `cars_library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` text COLLATE utf8_turkish_ci NOT NULL,
+  `password` text COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `admin`
+--
+
+INSERT INTO `admin` (`id`, `password`) VALUES
+('admin', 'admin'),
+('mes', 'mes');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
